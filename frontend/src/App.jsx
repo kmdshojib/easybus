bb3201e17c1c33c0c625a8bcc28255d4d5477a19
abhishek <abhiskg20@gmail.com>
2022-12-16 12:45:24 +0600

    MUI added with custom theme configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
+import { createTheme } from "@mui/material/styles";
+import { purple } from "@mui/material/colors";
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+  },
+});
 
 function App() {
   return (
     <div>
-      <RouterProvider router={router} />
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </div>
   );
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,10 @@
+import Button from "@mui/material/Button";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+  );
 };
 
 export default Home;
