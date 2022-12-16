145a4541d2f0abd9d449c707c994d14383edff43
abhishek <abhiskg20@gmail.com>
2022-12-16 12:11:06 +0600

    folder structure created

---
+++ frontend/src/layout/Main.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/ui/Header";
+import Footer from "../components/ui/Footer";
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Main;
