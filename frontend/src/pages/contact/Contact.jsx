837acf42960e5108e28fc59b899dac50d86a174b
kmdshojib <kmd.shojib@gmail.com>
2022-12-20 23:08:23 +0600

    destibation section added

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -1,5 +1,40 @@
+import { Box, Typography, FormControl, TextField,Button,Input  } from "@mui/material";
+
+
 const Contact = () => {
-  return <div></div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  }
+  return (
+    <Box>
+      <Box sx={{width:"100%",height:"243px"}}></Box>
+      <Box>
+        <Box marginTop={1} sx={{ textAlign:"center"}}>
+          <Typography variant="p">CONTACT US NOW</Typography>
+          <Typography variant="h5">Write a Message</Typography>
+        </Box>
+        <Box sx={{display:"flex",justifyContent:"center"}}>
+          <form onSubmit={handleSubmit}>
+            <Box>
+              <TextField name="name" id="outlined" placeholder="Your Name" variant="outlined" />
+              <TextField id="outlined-basic" placeholder="Your Email" variant="outlined" />
+            </Box>
+            <Box>
+              <TextField id="outlined-basic" placeholder="Subject" variant="outlined" />
+              <TextField id="outlined-basic" placeholder="Your Mobile" variant="outlined" />
+            </Box>
+            <Box  sx={{display:"flex",justifyContent:"center"}}>
+              <TextField id="outlined-basic" placeholder="Message" variant="outlined" />
+            </Box>
+            <Box  sx={{display:"flex",justifyContent:"center"}}>
+              <Button type="submit" variant="contained">Submit</Button>
+            </Box>
+          </form>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Contact;
