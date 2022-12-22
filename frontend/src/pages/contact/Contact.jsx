8b92fc1102f9b1ad0be8f2c2e5a3f183238ed841
kmdshojib <kmd.shojib@gmail.com>
2022-12-23 02:03:03 +0600

    final style added

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
             </Box>
           </Box>
         </Box>
-        <Box>
+        <Box marginTop={2}>
           <Box marginTop={1} sx={{ textAlign: "center" }}>
             <Typography sx={{ fontSize: "12px" }} variant="p">CONTACT US NOW</Typography>
             <Typography variant="h5">Write a Message</Typography>
@@ -31,15 +31,15 @@ const Contact = () => {
           <Box sx={{ display: "flex", justifyContent: "center" }}>
             <form onSubmit={handleSubmit}>
               <Box sx={{ marginBottom: "10px", marginTop: "15px" }}>
-                <TextField sx={{ marginRight: "10px", width: { xs: "300px", lg: "400px" }, marginBottom: "15px" }} label="Your Name" name="name" id="outlined" placeholder="Your Name" variant="outlined" /><br />
-                <TextField sx={{ marginRight: "10px", width: { xs: "300px", lg: "400px" }, marginBottom: "15px" }} id="outlined-basic" label="Your Email" placeholder="Your Email" variant="outlined" />
+                <TextField sx={{ marginRight: "10px", width: { xs: "300px", lg: "450px" }, marginBottom: "15px" }} label="Your Name" name="name" id="outlined" placeholder="Your Name" variant="outlined" /><br />
+                <TextField sx={{ marginRight: "10px", width: { xs: "300px", lg: "450px" }, marginBottom: "15px" }} id="outlined-basic" label="Your Email" placeholder="Your Email" variant="outlined" />
               </Box>
               <Box sx={{ marginBottom: "10px" }}>
-                <TextField sx={{ marginRight: "10px", width: { xs: "300px", lg: "400px" }, marginBottom: "15px" }} id="outlined-basic" label="Subject" placeholder="Subject" variant="outlined" /> <br />
-                <TextField sx={{ marginRight: "10px", width: { xs: "300px", lg: "400px" }, marginBottom: "15px" }} id="outlined-basic" label="Your Mobile" placeholder="Your Mobile" variant="outlined" /><br />
+                <TextField sx={{ marginRight: "10px", width: { xs: "300px", lg: "450px" }, marginBottom: "15px" }} id="outlined-basic" label="Subject" placeholder="Subject" variant="outlined" /> <br />
+                <TextField sx={{ marginRight: "10px", width: { xs: "300px", lg: "450px" }, marginBottom: "15px" }} id="outlined-basic" label="Your Mobile" placeholder="Your Mobile" variant="outlined" /><br />
               </Box>
               <Box sx={{ marginBottom: "10px" }}>
-                <TextareaAutosize style={{ width: 400, height: 100 }} name="Message" id="outlined-basic" label="Message" placeholder="Message" variant="outlined" />
+                <TextareaAutosize className="text-area" style={{ height: 100 }} name="Message" id="outlined-basic" label="Message" placeholder="Message" variant="outlined" />
               </Box>
               <Box sx={{ display: "flex", justifyContent: "center" }}>
                 <Button type="submit" variant="contained">Submit</Button>
