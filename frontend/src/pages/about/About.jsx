331db8ca70d2c72e7434a8657b43fb1ae5270613
Rasel Parvej <RaselParvejABC@gmail.com>
2022-12-23 16:51:15 +0600

    For Pull

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,5 +1,30 @@
+import { Box, Container, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
 const About = () => {
-  return <div></div>;
+  return (
+    <>
+      <Box
+        sx={{
+          height: "300px",
+          backgroundImage: "url('/about-poster.jpg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          variant="h5"
+          sx={{ fontSize: 40, color: "white", fontWeight: "bold" }}
+        >
+          About EasyBus
+        </Typography>
+      </Box>
+      <Container></Container>
+    </>
+  );
 };
 
 export default About;
