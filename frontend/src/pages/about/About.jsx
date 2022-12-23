7f0bb87529979570637144b0a0107f7107d10dd5
Rasel Parvej <RaselParvejABC@gmail.com>
2022-12-23 18:29:17 +0600

    About Page done except FAQ

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,18 +1,20 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const About = () => {
   return (
     <>
       <Box
         sx={{
-          height: "300px",
+          height: "250px",
           backgroundImage: "url('/about-poster.jpg')",
           backgroundSize: "cover",
           backgroundPosition: "center",
           display: "flex",
           justifyContent: "center",
-          alignItems: "center",
+          alignItems: "flex-end",
+          paddingBottom: "50px",
         }}
       >
         <Typography
@@ -22,7 +24,89 @@ const About = () => {
           About EasyBus
         </Typography>
       </Box>
-      <Container></Container>
+      <Container sx={{ marginY: "70px" }}>
+        <Grid container spacing={3} justifyContent="center">
+          <Grid item sm={12} md={6} sx={{ p: "40px" }}>
+            <Typography
+              variant="p"
+              sx={{ textTransform: "uppercase", fontWeight: "bold" }}
+              color="primary"
+            >
+              Who we are
+            </Typography>
+            <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+              Making Bus Journey in Bangladesh Hassle-Free
+            </Typography>
+            <Typography variant="body1" component="p" sx={{ my: "25px" }}>
+              Our mission is to ensure 'Hassle-free' and Easy Bus Journey in
+              Bangladesh. Our first goal is that our users must be able to book
+              a ticket in a fex minutes, away from all the hassle at a bus
+              counter, while enjoying the warmth of home, or while in some off
+              minutes among the busy hours of office.
+            </Typography>
+            {[
+              "Book or Cancel a Ticket Online!",
+              "Reserve a Bus!",
+              "Any Convenient Pick-point or Drop-point along the route!",
+            ].map((feature, index) => (
+              <Typography
+                key={index}
+                variant="body2"
+                sx={{
+                  marginY: "10px",
+                  fontSize: "16px",
+                  display: "flex",
+                  alignItems: "center",
+                  color: "#571336",
+                }}
+              >
+                <CheckCircleIcon
+                  fontSize="16px"
+                  sx={{
+                    marginRight: "10px",
+                  }}
+                />
+                <span>{feature}</span>
+              </Typography>
+            ))}
+            <Box
+              sx={{
+                display: "block",
+                marginTop: "40px",
+              }}
+            >
+              <Link to="/booking" style={{ textDecoration: "none" }}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: "#FFA903",
+                    textDecoration: "none",
+                  }}
+                >
+                  <span
+                    style={{
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Book a Ticket
+                  </span>
+                </Button>
+              </Link>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            sm={12}
+            md={6}
+            sx={{
+              backgroundImage: "url('/about-banner.jpg')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              borderRadius: "10px",
+            }}
+          ></Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
