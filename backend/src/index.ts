7161254006291df2901dd58f27994f43f711dccf
abhishek <abhiskg20@gmail.com>
2022-12-25 22:47:55 +0600

    My bookings routes created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import user from "./routes/user";
 import dbConnect from "./config/dbConnect";
 import location from "./routes/location";
 import bus from "./routes/bus";
+import offerDeals from "./routes/offerDeals";
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,7 @@ const port = process.env.PORT || 5000;
 app.use("/api/v1", user);
 app.use("/api/v1", location);
 app.use("/api/v1", bus);
+app.use("/api/v1", offerDeals);
 
 dbConnect()
   .then(() => {
