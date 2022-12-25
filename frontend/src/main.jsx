68ae16427e958a98362d1be627f8e59219e0b0db
abhishek <abhiskg20@gmail.com>
2022-12-25 09:14:47 +0600

    Initial redux setup complete

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,14 @@ import App from "./App";
 import "./index.css";
 import "swiper/css/bundle";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Toaster />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
