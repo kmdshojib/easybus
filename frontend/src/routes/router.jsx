b7b994ebda6e3243cf496420768eaef2e0684af0
IffatNur <iffatnoorshad00@gmail.com>
2022-12-28 20:36:37 +0600

    dashboard created

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -4,6 +4,7 @@ import Main from "../layout/Main";
 import About from "../pages/about/About";
 import Booking from "../pages/booking/Booking";
 import Contact from "../pages/contact/Contact";
+import AddNewBus from "../pages/dashboard/AddNewBus";
 import Dashboard from "../pages/dashboard/Dashboard";
 import ErrorPage from "../pages/error/ErrorPage";
 import Home from "../pages/home/Home";
@@ -55,6 +56,10 @@ const router = createBrowserRouter([
       {
         path: "/dashboard",
         element: <Dashboard />,
+      },
+      {
+        path: "/dashboard/addbus",
+        element: <AddNewBus />,
       },
     ],
   },
