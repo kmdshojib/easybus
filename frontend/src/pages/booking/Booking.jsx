10a3976a6abc2826d6259e6dde42ff4d4911c4aa
mitaly-dev <mitalyy.dev@gmail.com>
2022-12-28 21:50:29 -0700

    booking page created

--- frontend/src/pages/booking/Booking.jsx
+++ frontend/src/pages/booking/Booking.jsx
@@ -1,7 +1,22 @@
+import { useGetAllSeatQuery } from "../../features/bus/busSlice";
+import * as React from 'react';
+import {Box, Typography } from "@mui/material";
+import SingleBooking from "./SingleBooking";
+
 const Booking = () => {
-  const { data } = useGetAllSeatQuery();
-  const busTicket = data?.data;
-  return <div></div>;
+  const { data,isLoading } = useGetAllSeatQuery()
+  const bookings = data?.data;
+  console.log(bookings)
+  if(isLoading){
+    return <Typography>Loading...</Typography>
+  }
+  return  <Box sx={{py:"30px",backgroundColor:"#FBF9F2",height:"80vh",mt:"80px"}}>
+     {
+     bookings?.map((booking) =><SingleBooking key={booking._id} booking={booking}></SingleBooking>)
+    }
+  </Box> ;
 };
 
 export default Booking;
+
+
