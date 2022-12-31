f0b97373833aa36b2783dad38deed77658445662
abhishek <abhiskg20@gmail.com>
2022-12-31 11:57:39 +0600

    User role check route added

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import User from "../models/User";
 import { AssignJwt } from "../services/ManageJwt";
+import { QueryUser } from "../services/UserService";
 
 export const CreateNewUser = async (req: Request, res: Response) => {
   if (req.body?.role && req.body.role === "admin") {
@@ -22,6 +23,24 @@ export const GetAllUsers = async (req: Request, res: Response) => {
   try {
     const allUsers = await User.find({});
     res.status(200).json({ success: true, data: allUsers });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: (error as Error).message,
+    });
+  }
+};
+
+export const GetUserRole = async (req: Request, res: Response) => {
+  const decodedEmail = req.decoded.email;
+  try {
+    const user = await QueryUser(decodedEmail);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    res.status(200).json({ success: true, data: user.role });
   } catch (error) {
     res.status(400).json({
       success: false,
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -3,13 +3,16 @@ import {
   CreateNewUser,
   DeleteUser,
   GetAllUsers,
+  GetUserRole,
 } from "../controllers/UserController";
 import { CheckExistingUser } from "../middlewares/CheckExistingUser";
 import ValidateId from "../middlewares/ValidateId";
+import { VerifyJwt } from "../middlewares/VerifyJwt";
 
 const router = express.Router();
 
 router.get("/users", CheckExistingUser, GetAllUsers);
+router.get("/user/role", VerifyJwt, GetUserRole);
 router.post("/user/new", CreateNewUser);
 router.delete("/user/:id", ValidateId, DeleteUser);
 
