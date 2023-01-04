6978672914192cfbb14bd676dc7dc0f9f8d59887
mitaly-dev <mitalyy.dev@gmail.com>
2023-01-04 08:01:25 -0700

    skeleton added in offerDeals section

--- frontend/src/pages/home/sections/OfferDeals/OfferDeals.jsx
+++ frontend/src/pages/home/sections/OfferDeals/OfferDeals.jsx
@@ -12,29 +12,88 @@ const OfferDeals = () => {
 
   if(isLoading){
     return (
-      <Grid container spacing={3} justifyContent="center" sx={{ p: "30px"}}>
-        {new Array(4).fill(0).map((route, index) => (
-          <Grid key={index} item  xs={12} md={6} lg={3}>
-            <Skeleton variant="rectangular" height={200} />
-            <Skeleton />
-            <Skeleton />
-            <Skeleton variant="text" height={32} sx={{ my: "16px" }} />
-            <Skeleton
-              variant="rectangular"
-              height={40}
-              sx={{
-                width: "80%",
-                height: "200px",
-                borderRadius: "15px",
-                mx: "auto",
-              }}
-            />
+      <Box>
+      <Container sx={{ py: "30px", backgroundColor: "#FBF9F2" }}>
+        <Box>
+          <Typography variant="h6" fontSize={16} color={"#FFA903"}>
+            OFFER DEALS
+          </Typography>
+          <Typography variant="h4" fontWeight={600} marginBottom={4}>
+            Bus Offer Deals
+          </Typography>
+        </Box>
+        <Grid
+          container
+          maxWidth={{ xs: "100%", md: "80%", lg: "100%" }}
+          spacing={{ xs: 0, md: 1 }}
+          paddingX={0}
+          marginX={{ xs: 0, md: "auto" }}
+        >
+          <Grid item lg={6} xs={12} width="100%" paddingX={0}>
+            {new Array(1).fill(0).map((deal,index) => (
+              <Grid key={index} item 
+              >
+                  <Skeleton variant="rectangular" height={200} />
+                  <Skeleton />
+                  <Skeleton />
+                  <Skeleton variant="text" height={32} sx={{ my: "16px" }} />
+                  <Skeleton
+                    variant="rectangular"
+                    height={40}
+                    sx={{
+                      width: "80%",
+                      height: "200px",
+                      borderRadius: "15px",
+                      mx: "auto",
+                    }}
+                  />
+                </Grid>
+            ))}
           </Grid>
-        ))}
-      </Grid>
+          <Grid item lg={6} xs={12} width="100%" paddingX={0} marginX={0}>
+            <Grid
+              container
+              width="100%"
+              spacing={{ xs: 0, md: 1 }}
+              paddingX={0}
+              marginX={0}
+            >
+              {new Array(4).fill(0).map((deal,index) => (
+                 <Grid key={index} item 
+                 xs={12}
+                 sm={6}
+                 width="100%"
+                 paddingX={0}
+                 marginX={0}
+                 >
+                    <Skeleton variant="rectangular" height={200} />
+                    <Skeleton />
+                    <Skeleton />
+                    <Skeleton variant="text" height={32} sx={{ my: "16px" }} />
+                    <Skeleton
+                      variant="rectangular"
+                      height={40}
+                      sx={{
+                        width: "80%",
+                        height: "200px",
+                        borderRadius: "15px",
+                        mx: "auto",
+                      }}
+                    />
+                  </Grid>
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
     )
   }
 
+  
+
+
+  
   return (
     <Box>
       <Container sx={{ py: "30px", backgroundColor: "#FBF9F2" }}>
